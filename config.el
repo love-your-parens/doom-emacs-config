a29bc0771bc3f2fb7a381b8e9db61530f592c0b2
Konrad Wątor <konrad@wator.it>
2024-08-01 16:24:22 +0200

    Update config.el

--- config.el
+++ config.el
@@ -6,16 +6,42 @@
       doom-variable-pitch-font (font-spec :family "Iosevka Aile" 
                                           :weight 'regular))
 
-;; Resize & reposition the window in GUI mode:
-(when window-system
- (set-frame-size (selected-frame) 120 60)
- (modify-frame-parameters (selected-frame) '((top . 0.5) (left . 0.5))))
 
-;; Doom splash screen logo
-;; (setq fancy-splash-image (concat doom-user-dir "splash/doomEmacs.svg"))
+;;; General settings
+(after! treemacs (treemacs-follow-mode 1))
 
-;; Editor functionality
+;; Display
+(setq default-frame-width 150)
+(setq default-frame-height 60)
+
+;; LSP
+(setenv "LSP_USE_PLISTS" "true")
+(setq lsp-headerline-breadcrumb-enable t
+      lsp-intelephense-licence-key ""
+      lsp-file-watch-threshold nil)
+
+
+;;; Editor functionality
+(defun set-default-frame-size-and-position (&optional frame)
+  (set-frame-size frame
+                  default-frame-width
+                  default-frame-height)
+  (modify-frame-parameters frame
+                           '((left . 0.5)
+                             (top . 0.5))))
+
+(let ((decorated t))
+  (defun toggle-frame-decoration (&optional frame)
+    "Toggles frame decoration on/off."
+    (interactive)
+    (modify-frame-parameters frame
+                             (cons (cons 'undecorated decorated)
+                                   '((left . 0.5)
+                                     (top . 0.5))))
+    (setq decorated (not decorated))))
+
 (defun window-next-enlargen ()
+  "Switches to the next window and enlarges it."
   (interactive)
   (evil-window-next nil)
   (doom/window-enlargen)
@@ -35,22 +61,26 @@
   (interactive "^p")
   (forward-same-syntax (- (or arg 1))))
 
+
 ;;; Keybindings
 ;; Editor
+(map! :leader :desc "Toggle frame decoration" "t d" #'toggle-frame-decoration)
 (map! :nvi "M-<left>" #'backward-same-syntax)
 (map! :nvi "M-<right>" #'forward-same-syntax)
 (map! :leader "w O" #'window-next-enlargen)
 (map! "M-<backspace>" #'evil-delete-backward-word)
 (map! "C-\"" #'paredit-doublequote)
 
+
 ;; Structural editing
 (map! "S-<down>" #'sp-up-sexp)
-(map! "S-<up>" #'sp-backward-up-sexp)
+(map! "S-<up>" #'sp-backward-down-sexp)
 (map! "C-M-," #'sp-backward-slurp-sexp)
 (map! "C-M-." #'sp-forward-slurp-sexp)
 (map! "M-9" #'sp-wrap-round)
 (map! "M-[" #'sp-wrap-square)
 (map! "M-{" #'sp-wrap-curly)
+
 
 ;; DAP Debugging
 (map! :leader
@@ -58,6 +88,7 @@
        :desc "Toggle breakpoint" "t" #'dap-breakpoint-toggle
        :desc "Start debugging" "d" #'dap-debug
        :desc "Stop debugging" "c" #'dap-disconnect))
+
 
 ;; CIDER
 (map! :after cider
@@ -70,6 +101,7 @@
       :localleader
       "e s" #'cider-eval-sexp-at-point)
 
+
 ;; PHP
 (map! :after php-mode
       :map php-mode-map
@@ -77,8 +109,9 @@
       "c f" #'lsp-format-buffer
       "c F" #'lsp-format-region)
 
-(add-hook 'php-mode-hook 'lsp-headerline-breadcrumb-mode)
 
-;; LSP
-;;; INTELEPHENSE license key
-(setq lsp-intelephense-licence-key "")
+;;; Frame setup.
+(when window-system
+  (set-default-frame-size-and-position)
+  (add-hook 'after-make-frame-functions
+            #'set-default-frame-size-and-position))
