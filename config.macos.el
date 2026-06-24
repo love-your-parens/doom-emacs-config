0d36241d95132981070a50fbb67bb29e8d946531
Konrad Wątor <konrad@wator.it>
2026-06-24 14:48:20 +0200

    Automate integration for MacOS + Eglot + Lua LSP
    Assumes that Homebrew is used to install Sumineko's LUA language server.

--- config.macos.el
+++ config.macos.el
@@ -14,3 +14,20 @@
 (setq shell-file-name (executable-find "zsh"))
 (setq-default vterm-shell "/opt/homebrew/bin/fish")
 (setq-default explicit-shell-file-name "/opt/homebrew/bin/fish")
+
+;; NOTE Assume that if Lua LSP is in use, it will have been installed using Homebrew.
+;; For that to work with Eglot, default paths need to be changed. In particular:
+;; `+lua-lsp-dir' needs to be pointing to the same directory that `main.lua' is in.
+;; Since this path depends on versioning, we must locate it by pattern.
+;; NOTE Observed to add ~10ms to the startup.
+(when (and (modulep! :lang lua +lsp)
+           (modulep! :tools lsp +eglot))
+  (let ((lua-server-path
+         (let ((filename (with-temp-buffer
+                           (call-process-shell-command
+                            "find /opt/homebrew/Cellar/lua-language-server/ -path '*/libexec/main.lua' 2>/dev/null | head -n1"
+                            nil t)
+                           (buffer-string))))
+           (when (not (string-empty-p filename))
+             (file-name-parent-directory filename)))))
+    (setq +lua-lsp-dir lua-server-path)))
